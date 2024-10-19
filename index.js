f329fa516992e0b0b3bbbe3b67a3aff78ca40c4c
Shane S <76753390+ShaneCoding@users.noreply.github.com>
2024-10-19 14:47:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["✈️ Watching the Escape Room", "🔐 Good luck!"];
+const statusMessages = ["🔐 Listening for codes...", "✈️ Good luck!"];
 const statusTypes = ['online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
